176a879ae16ccf67d8dafcb73b4157b23d1e0698
Joe Higham <jphigham.ftc@gmail.com>
2021-05-12 14:27:56 -0400

    Add data_api.h

---
+++ data_api.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <string>
+
+static std::string home_json_url("https://cd-static.bamgrid.com/dp-117731241344/home.json");
+static std::string ref_json_base_url("https://cd-static.bamgrid.com/dp-117731241344/sets/");
